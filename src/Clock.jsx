4e0f91574d2e79f3db4a8f1be268ea5419186fb4
Ross Freeman <rfreeman1@me.com>
2017-01-20 17:45:33 -0500

    Add styling

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -20,6 +20,10 @@ class Clock extends Component {
     setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
   }
 
+  leading0(num) {
+    return num < 10 ? '0' + num : num;
+  }
+
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
     const seconds = Math.floor((time/1000) % 60);
@@ -33,10 +37,10 @@ class Clock extends Component {
   render() {
     return(
       <div>
-        <div className="Clock-days">{this.state.days} Days</div>
-        <div className="Clock-hours">{this.state.hours} Hours</div>
-        <div className="Clock-minutes">{this.state.minutes} Minutes</div>
-        <div className="Clock-seconds">{this.state.seconds} Seconds</div>
+        <div className="Clock-days">{this.leading0(this.state.days)} Days</div>
+        <div className="Clock-hours">{this.leading0(this.state.hours)} Hours</div>
+        <div className="Clock-minutes">{this.leading0(this.state.minutes)} Minutes</div>
+        <div className="Clock-seconds">{this.leading0(this.state.seconds)} Seconds</div>
       </div>
     )
   }
